3f0105e83c44cd72bff0aa90135a69225783df8b
Panshi <shix_112@126.com>
2021-11-10 15:57:18 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # panshishuo.github.io
 
----
-
-[org mode notes root](/public/index.html)
--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/sh
 
+rm -rf ./public index.html
+
 emacs -Q --script build_site.el
+
+cp ./public/index.html index.html
