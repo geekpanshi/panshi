aa08e6247e6d4d9b9103462c9a3e8e39ae7c9b15
Panshi <shix_112@126.com>
2021-11-10 16:16:40 +0800

    update

--- build_site.el
+++ build_site.el
@@ -34,8 +34,8 @@
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
              :with-author t
-             :with-creator nil            ;; Include Emacs and Org versions in footer
-             :with-toc t                ;; Include a table of contents
+             :with-creator t            ;; Include Emacs and Org versions in footer
+             :with-toc nil                ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
              :time-stamp-file nil)))    ;; Don't include time stamp in file
 
