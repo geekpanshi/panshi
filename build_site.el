e34fb06de05c0fd14275781f9317a83b66ecce10
Panshi <shix_112@126.com>
2021-12-09 15:44:37 +0800

    update config

--- build_site.el
+++ build_site.el
@@ -25,6 +25,13 @@
       org-html-head-include-default-style nil ;; Use our own styles
       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
+(defun org-publish-sitemap-time-entry (entry style project)
+    (format "%s %s"
+            (format-time-string
+             "[%Y.%m.%d]"
+             (org-publish-find-date entry project))
+            (org-publish-sitemap-default-entry entry style project)))
+
 ;; Define the publishing project
 (setq org-publish-project-alist
       `(("org-post"
@@ -33,15 +40,21 @@
              :base-extension "org"
              :publishing-function org-html-publish-to-html
              :publishing-directory "./public"
+             :exclude "sitemap.org"
              :with-creator t            ;; Include Emacs and Org versions in footer
-             :with-toc t                ;; Include a table of contents
+             :with-toc nil              ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
              :time-stamp-file nil       ;; Don't include time stamp in file
+             :auto-preamble t
              :auto-sitemap t
+             :author "panshishuo"
+             :html-link-home "/"
+             :html-link-up "/"
              :sitemap-style list
              :sitemap-title "磐石说"
              :sitemap-filename "index.org"
              :sitemap-sort-files anti-chronologically
+             :sitemap-format-entry org-publish-sitemap-time-entry
              :html-head ,(concat
                           "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"/>\n"
                           "<script src=\"https://hm.baidu.com/hm.js?4dbc75a8d627e17a8714e4c8b2e9afa8\"></script>")
